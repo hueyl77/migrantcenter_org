5793f06f919dc4ef76302b12379dde5384bdf771
Huey Ly <huey@webmail.us>
2017-08-08 19:53:29 -0500

    updated mobile header

--- wp-content/themes/alone/templates/headers/header-mobi.php
+++ wp-content/themes/alone/templates/headers/header-mobi.php
@@ -12,14 +12,26 @@ $header_class_arr = array( basename( __FILE__, '.php' ), $alone_logo_retina, 'fw
 	<div class="bt-header-mobi-main">
 		<div class="container">
 			<div class="bt-container-logo bt-vertical-align-middle">
-				<?php alone_logo(); ?>
+				<table class="borderless" style="width: 100%;">
+					<tr>
+						<td>
+							<a href="/"><img src="/wp-content/uploads/2017/08/mc4hr-logo.png"
+							style="width: 80px; margin-top: 10px;" border="0"
+							alt="MCHR" /></a>
+						</td>
+						<td style="text-align: center;">
+							<a href="/">MIGRANT CENTER FOR HUMAN RIGHTS</a>
+						</td>
+					</tr>
+				</table>
 			</div><!--
-			--><div class="bt-container-menu bt-vertical-align-middle">
+			--><div class="bt-container-menu bt-vertical-align-middle" style="width: 100%;">
 				<div class="bt-nav-wrap" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement" role="navigation">
 					<?php
 					if( ! $_FW ) { ?>
 						<div class="default-mobi-menu-wrap" data-default-menu-mobi-handle>
-							<button class="button-toggle-ui"><?php _e('Menu', 'alone'); ?></button>
+							<button class="button-toggle-ui" style="max-width: 70%; min-width: 70%; background: #0d430d; border: none;"><?php _e('Menu', 'alone'); ?></button>
+							<a class="btn-style2" href="/contact-us">DONATE</a>
 							<?php fw_theme_nav_menu( 'mobi_menu' ); ?>
 						</div>
 					<?php } else {
